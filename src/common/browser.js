fa2c6c9fe69414850674c2460becf2baa50ef7c8
Ryan Choo <blazerod123480@gmail.com>
2025-05-06 14:10:09 +0800

    gg

--- src/common/browser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isMobile = void 0;
-function isMobile() {
-    const ua = navigator.userAgent;
-    return ua.includes("Android") || ua.includes("iPhone") || ua.includes("iPad");
-}
-exports.isMobile = isMobile;
